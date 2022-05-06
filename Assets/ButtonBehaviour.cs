00b57df4ce298e0951deada973d7b7285b082b7b
solparrondo <101563449+solparrondo@users.noreply.github.com>
2022-05-06 14:32:22 -0300

    Buttons, while, cubes (6-5)

---
+++ Assets/ButtonBehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonBehaviour : MonoBehaviour
+{
+
+    public void ShowMessageConsole()
+    {
+        Debug.Log("Hello World");
+    }
+        
+
+}
---
+++ Assets/instatiator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instatiator : MonoBehaviour
+{
+
+    public GameObject objectToClone;
+
+    public void Multipilicar()
+    {
+        int counter = 0;
+        while (counter < 3)
+        {
+
+            Instantiate(objectToClone);
+            counter++;
+
+        }
+    }
+        // Start is called before the first frame update
+        void Start()
+    {
+
+     
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
